b290fe27340ffd522346920db6b209d0bbb205a4
BryceeL <bcllamc280@gmail.com>
2024-09-10 21:42:12 -0700

    created from vite react template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className = "MainApp">
+      <h1>Soon to be FrontEnd of Social Media</h1>
+    </div>
+  )
+}
+
+export default App
